2e2c68a462f2f1547215d689af4a05e1606f7361
DaveRoox <davexrusso@gmail.com>
2017-06-14 23:16:04 +0200

    Create NumberComplement.cpp

---
+++ NumberComplement.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+    public:
+        int findComplement(int num) {
+            int res = ~num, bits = 1;
+            while(num >>= 1)
+                bits += 1;
+            return res & ((1 << bits) - 1);
+        }
+};
+
+int main(int argc, const char * argv[]) {
+    
+    int input = 5;
+    
+    cout << Solution().findComplement(input) << endl;
+    return 0;
+}
