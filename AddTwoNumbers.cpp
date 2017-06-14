7bc6572e5d0a39737ef2d0412099a0b206da4cda
DaveRoox <davexrusso@gmail.com>
2017-06-14 20:33:34 +0200

    Create AddTwoNumbers.cpp

--- AddTwoNumbers.cpp
+++ AddTwoNumbers.cpp
@@ -9,49 +9,48 @@ struct ListNode {
 };
 
 class Solution {
-
-  public:
-      void addTo(ListNode *&l, int value) {
-          ListNode *tmpList = l;
-          if(tmpList) {
-              while(tmpList->next)
-                  tmpList = tmpList->next;
-              tmpList->next = new ListNode(value % 10);
+	
+    private:
+        void addTo(ListNode *&l, int value) {
+              ListNode *tmpList = l;
+              if(tmpList) {
+                  while(tmpList->next)
+                      tmpList = tmpList->next;
+                  tmpList->next = new ListNode(value % 10);
+              }
+              else
+                  l = new ListNode(value % 10);
           }
-          else
-              l = new ListNode(value % 10);
-      }
+	
+    public:
+		ListNode* addTwoNumbers(ListNode *l1, ListNode *l2) {
 
-      ListNode* addTwoNumbers(ListNode *l1, ListNode *l2) {
+			ListNode *tmp1 = l1, *tmp2 = l2, *newList = 0;
+			short carry = 0;
 
-          ListNode *tmp1 = l1;
-          ListNode *tmp2 = l2;
-          ListNode *newList = 0;
-          short carry = 0;
+			while(tmp1 || tmp2) {
+				short val = (tmp1 ? tmp1->val : 0) + (tmp2 ? tmp2->val : 0) + carry;
+				addTo(newList, val);
+				carry = val >= 10;
+				if(tmp1)
+					tmp1 = tmp1->next;
+				if(tmp2)
+					tmp2 = tmp2->next;
+			}
 
-          while(tmp1 || tmp2) {
-              short val = (tmp1 ? tmp1->val : 0) + (tmp2 ? tmp2->val : 0) + carry;
-              addTo(newList, val);
-              carry = val >= 10;
-              if(tmp1)
-                  tmp1 = tmp1->next;
-              if(tmp2)
-                  tmp2 = tmp2->next;
-          }
+			if(carry)
+				addTo(newList, carry);
 
-          if(carry)
-              addTo(newList, carry);
-
-          return newList;
-      }
+			return newList;
+		}
 };
 
 void print(ListNode *ln) {
-    ListNode *tmp = ln;
-    while(tmp) {
-        cout << tmp->val;
-        tmp = tmp->next;
-    }
+	ListNode *tmp = ln;
+	while(tmp) {
+		cout << tmp->val;
+		tmp = tmp->next;
+	}
 }
     
 int main(int argc, const char * argv[]) {
