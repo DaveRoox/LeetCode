eec2c04d1970112412092798a2be393f1c70d8f2
DaveRoox <davexrusso@gmail.com>
2017-06-15 22:36:34 +0200

    Create BullsAndCows.cpp

--- BullsAndCows.cpp
+++ BullsAndCows.cpp
@@ -4,23 +4,25 @@
 using namespace std;
 
 class Solution {
+    
     public:
         string getHint(string secret, string guess) {
             
             int cows = 0, bulls = 0, histogram[10] = {0};
-            for(auto c : secret)
-                histogram[c - '0']++;
-            
-            for(int i = 0; i < guess.length(); i++) {
-                if(guess[i] == secret[i])
-                    bulls += 1;
-                if(histogram[guess[i] - '0'] != 0) {
-                    histogram[guess[i] - '0'] -= 1;
-                    cows += 1;
+
+            for(int i = 0; i < secret.length(); i++)
+                if(secret[i] != guess[i])
+                    histogram[secret[i] - '0']++;
+                else
+                    bulls++;
+
+            for(int i = 0; i < guess.length(); i++)
+                if(histogram[guess[i] - '0'] && guess[i] != secret[i]) {
+                    histogram[guess[i] - '0']--;
+                    cows++;
                 }
-            }
-            
-            return std::to_string(bulls) + "A" + std::to_string(cows - bulls) + "B";
+
+            return std::to_string(bulls) + "A" + std::to_string(cows) + "B";
         }
 };
 
